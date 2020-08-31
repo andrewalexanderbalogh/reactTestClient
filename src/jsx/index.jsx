1cddcf93979d8f809e6ffcdce3a2c7196e9215ac
Andrew Alexander Balogh <andrewalexanderbalogh@gmail.com>
2020-08-31 15:45:24 -0400

    basic employee fetch table

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,3 +1,6 @@
+/**
+ * Produce a Table listing Departments Data
+ */
 class DepartmentsTable extends React.Component {
     constructor(props) {
         super(props);
@@ -41,6 +44,9 @@ class DepartmentsTable extends React.Component {
 }
 
 
+/**
+ * Produce Appropriate Data for Departments Section
+ */
 class DepartmentsSection extends React.Component {
     constructor(props) {
         super(props);
@@ -102,6 +108,128 @@ class DepartmentsSection extends React.Component {
 }
 
 
+/**
+ * Produce a Table listing Employees Data
+ */
+class EmployeesTable extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const employees = this.props.employees.map(employee => {
+            return (
+                <tr key={employee.emp_no}>
+                    <td>{employee.emp_no}</td>
+                    <td>{employee.birth_date}</td>
+                    <td>{employee.first_name}</td>
+                    <td>{employee.last_name}</td>
+                    <td>{employee.gender}</td>
+                    <td>{employee.hire_date}</td>
+                </tr>
+            );
+        });
+
+
+        return (
+            <table>
+                <caption>
+                    The Employees
+                </caption>
+
+                <colgroup>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                    <col/>
+                </colgroup>
+
+                <thead>
+                    <tr>
+                        <th scope="col">Emp_No</th>
+                        <th scope="col">Birth_Date</th>
+                        <th scope="col">First_Name</th>
+                        <th scope="col">Last_Name</th>
+                        <th scope="col">Gender</th>
+                        <th scope="col">Hire_Date</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {employees}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+
+/**
+ * Produce Appropriate Data for Departments Section
+ */
+class EmployeesSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { employees: [] };
+
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    fetchData(){
+        let _this = this;
+        const request = new Request('http://express.imprefvicticious.ca/employees?gender=M&hire_date=2020-01-01');
+        const reqInit = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+        };
+
+
+        fetch(request, reqInit)
+            .then(response => {
+                if (response.status === 200){
+                    return response.json();
+                }
+                else {
+                    throw new Error('Network response was not ok.');
+                }
+            })
+            .then(results => {
+                _this.setState({
+                    employees: results
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        let employeeContent;
+        if (this.state.employees.length){
+            employeeContent = <EmployeesTable employees={this.state.employees}/>;
+        }
+        else {
+            employeeContent = <aside>No Employee Data</aside>;
+        }
+
+
+        return (
+            <div>
+                <button onClick={this.fetchData}>
+                    Fetch Employee Data
+                </button>
+
+                {employeeContent}
+            </div>
+        );
+    }
+}
+
+
 /* Departments Section Content */
 ReactDOM.render(
     <DepartmentsSection/>,
@@ -110,3 +238,7 @@ ReactDOM.render(
 
 
 /* Employee Section Content */
+ReactDOM.render(
+    <EmployeesSection/>,
+    document.getElementById('employees')
+);
